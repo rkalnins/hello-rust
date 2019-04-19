796879c0cc63b907ae1708e8f9d33319a8b9dcd2
Roberts Kalnins <rkalnins314@gmail.com>
2019-04-19 12:31:49 -0400

    Add cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
+use structopt::StructOpt;
+
 
 fn main() {
+    let input = std::env::args().nth(1).expect("no expression provided");
+    let args = Cli {
+        input,
+    };
+
     let stdout = stdout();
-    let out = b"Hello fellow Rustaceans";
-    let width = 24;
+    let width = args.input.len() + 2;
 
     let mut writer = BufWriter::new(stdout.lock());
-    say(out, width, &mut writer).unwrap();
+    say(args.input.as_ref(), width, &mut writer).unwrap();
+}
+
+#[derive(StructOpt)]
+struct Cli {
+    input: String,
 }
